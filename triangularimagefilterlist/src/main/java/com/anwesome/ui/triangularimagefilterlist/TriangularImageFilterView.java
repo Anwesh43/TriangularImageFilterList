353820e1ab891b4a8820b833b7d2d485b7a2b0d4
Anwesh43 <anweshthecool0@gmail.com>
2017-05-17 04:04:32 +0530

    adding bitmap

--- triangularimagefilterlist/src/main/java/com/anwesome/ui/triangularimagefilterlist/TriangularImageFilterView.java
+++ triangularimagefilterlist/src/main/java/com/anwesome/ui/triangularimagefilterlist/TriangularImageFilterView.java
@@ -1,7 +1,9 @@
 package com.anwesome.ui.triangularimagefilterlist;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -9,13 +11,27 @@ import android.view.View;
  * Created by anweshmishra on 17/05/17.
  */
 public class TriangularImageFilterView extends View {
-    public TriangularImageFilterView(Context context) {
+    private int color = Color.WHITE;
+    private Bitmap bitmap;
+    private int time = 0,w,h;
+    public TriangularImageFilterView(Context context, Bitmap bitmap) {
         super(context);
     }
+    public void setColor(int color) {
+        this.color = color;
+    }
     public void onDraw(Canvas canvas) {
-
+        if(time == 0) {
+            w = canvas.getWidth();
+            h = canvas.getHeight();
+            bitmap = Bitmap.createScaledBitmap(bitmap,w,h,true);
+        }
+        time++;
     }
     public boolean onTouchEvent(MotionEvent event) {
         return true;
+    }
+    private class TriangularImage {
+        
     }
 }
