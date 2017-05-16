f8b595390412d39f4b554c7b58c93a50e540c504
Anwesh43 <anweshthecool0@gmail.com>
2017-05-17 04:31:35 +0530

    Adding OnSelectionChangeListener

---
+++ triangularimagefilterlist/src/main/java/com/anwesome/ui/triangularimagefilterlist/OnSelectionChangeListener.java
@@ -0,0 +1,9 @@
+package com.anwesome.ui.triangularimagefilterlist;
+
+/**
+ * Created by anweshmishra on 17/05/17.
+ */
+public interface OnSelectionChangeListener {
+    void onSelect();
+    void onUnSelect();
+}
