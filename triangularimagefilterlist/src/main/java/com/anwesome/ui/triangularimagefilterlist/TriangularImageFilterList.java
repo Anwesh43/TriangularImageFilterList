ee21f1bfdd21765e9bff48f4454efddea70470c8
Anwesh43 <anweshthecool0@gmail.com>
2017-05-17 11:41:07 +0530

    Using ListLayout in TriangularImageFilterList

--- triangularimagefilterlist/src/main/java/com/anwesome/ui/triangularimagefilterlist/TriangularImageFilterList.java
+++ triangularimagefilterlist/src/main/java/com/anwesome/ui/triangularimagefilterlist/TriangularImageFilterList.java
@@ -2,6 +2,7 @@ package com.anwesome.ui.triangularimagefilterlist;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.view.ViewGroup;
 import android.widget.ScrollView;
 
 /**
@@ -11,13 +12,16 @@ public class TriangularImageFilterList  {
     private Activity activity;
     private boolean isShown = false;
     private ScrollView scrollView;
+    private ListLayout listLayout;
     public TriangularImageFilterList(Activity activity) {
         this.activity = activity;
         scrollView = new ScrollView(activity);
+        listLayout = new ListLayout(activity);
+        scrollView.addView(listLayout,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
     }
     public void addImage(Bitmap bitmap,OnSelectionChangeListener onSelectionChangeListener) {
         if(!isShown) {
-
+            listLayout.addImage(bitmap,onSelectionChangeListener);
         }
     }
     public void show() {
